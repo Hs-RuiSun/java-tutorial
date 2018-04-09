9c9aed55ce3c71cd0e119804a8b9430882b6dadf
ruby <hs.ruisun@gmail.com>
2018-04-09 11:45:19 -0300

    two-way one-thread socket chat

---
+++ src/com/ruby/sun/socket/Client.java
@@ -0,0 +1,39 @@
+package com.ruby.sun.socket;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client {
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		Socket client = new Socket("127.0.0.1", 3000);
+		BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter pwrite = new PrintWriter(client.getOutputStream(), true);
+
+		BufferedReader receiveRead = new BufferedReader(new InputStreamReader(client.getInputStream()));
+
+		System.out.println("Start the chitchat, type and press Enter key");
+
+		String receiveMessage, sendMessage="";
+		while (!sendMessage.equals("exit")) {
+			System.out.print("client: ");
+			sendMessage = keyRead.readLine(); 
+			pwrite.println(sendMessage); 
+			pwrite.flush();
+			if ((receiveMessage = receiveRead.readLine()) != null) {
+				System.out.println("server: " + receiveMessage);
+			}
+		}
+		receiveRead.close();
+		keyRead.close();
+		pwrite.close();
+		client.close();
+	}
+}
---
+++ src/com/ruby/sun/socket/Server.java
@@ -0,0 +1,40 @@
+package com.ruby.sun.socket;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+	public static void main(String[] args) throws IOException {
+		ServerSocket server = new ServerSocket(3000);
+		System.out.println("Server  ready for chatting");
+		Socket sock = server.accept();
+		BufferedReader keyRead = new BufferedReader(new InputStreamReader(System.in));
+		PrintWriter pwrite = new PrintWriter(sock.getOutputStream(), true);
+
+		BufferedReader receiveRead = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+
+		String receiveMessage, sendMessage;
+		while (true) {
+			if ((receiveMessage = receiveRead.readLine()) != null) {
+				if(receiveMessage.equals("exit"))
+					System.out.println("client has exit");
+				else
+					System.out.println("client: " + receiveMessage);
+			}
+			System.out.print("server: ");
+			sendMessage = keyRead.readLine();
+			pwrite.println(sendMessage);
+			pwrite.flush();
+		}
+	}
+}
