322b11178c9fef649338412e2efdcdcb53f40317
ruby.sun <ruby.sun@cgi.com>
2019-01-09 17:47:16 -0400

    6. add JString

--- src/main/java/com/ruby/sun/jclass/JString.java
+++ src/main/java/com/ruby/sun/jclass/JString.java
@@ -2,6 +2,10 @@ package com.ruby.sun.jclass;
 
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.Arrays;
 
 public class JString {
@@ -16,5 +20,9 @@ public class JString {
             field = field.replaceAll("(\n|\r)", "yyy");
         }
         System.out.println(Arrays.toString(fields));
+    }
+    
+    public static void main(String[] args) throws FileNotFoundException {
+        BufferedReader br = new BufferedReader(new FileReader(new File("/c/Users/ruby.sun/Downloads/")));
     }
 }
