04c1b0f6bc61a28b8c63acfe6cdcfb67c8d7f3af
ruby.sun <ruby.sun@cgi.com>
2019-04-17 10:45:02 -0300

    add testImmutable

--- src/main/java/com/ruby/sun/collection/JCollection.java
+++ src/main/java/com/ruby/sun/collection/JCollection.java
@@ -15,10 +15,42 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class JCollection {
-    public void testArrayCopy() {
-        byte[] src = new byte[100];
-        byte[] des = new byte[100];
-        System.arraycopy(des, 0, src, 0, src.length);
+    /**
+     * JDK 9
+     * Immutable: Elements cannot be added or removed. Calling any mutator method will always cause UnsupportedOperationException to be thrown
+     *
+     * No null Element Allowed: Attempts to create them with null elements result in NullPointerException.
+     * In the case of List and Set, no elements can be null. In the case of a Map, neither keys nor values can be null.
+     *
+     * Value-Based Instances: If we create Lists with the same values, they may or may not refer to the same object on the heap.
+     *
+     * Serialization: They are serializable if all elements are serializable.
+     *
+     * Iteration Order: The iteration order of elements is unspecified and is subject to change.
+     */
+    @Test
+    public void testImmutable(){
+        String[] array = new String[]{"a", "b", "c"};
+
+        List<String> list1 = Arrays.asList(array);
+        //list1.add("d");// UnsupportedOperationException
+        assertArrayEquals(new String[]{"a", "b", "c"}, list1.toArray());
+        array[0] = "d";
+        assertArrayEquals(new String[]{"d", "b", "c"}, list1.toArray());
+
+        List<String> ilist1 = List.of(array);
+        assertArrayEquals(new String[]{"d", "b", "c"}, ilist1.toArray());
+        array[0] = "a";
+        assertArrayEquals(new String[]{"d", "b", "c"}, ilist1.toArray());
+
+        List<String> list2 = new ArrayList<>();
+        list2.add("a");
+        list2.add("b");
+        List<String> ilist2 = Collections.unmodifiableList(list2);
+        assertArrayEquals(new String[]{"a", "b"}, ilist2.toArray());
+        list2.add("c");
+        //ilist2.add("e"); //UnsupportedOperationException
+        assertArrayEquals(new String[]{"a", "b", "c"}, ilist2.toArray());
     }
 
     @Test
@@ -44,6 +76,8 @@ public class JCollection {
         Set<Integer> numbers = new HashSet<>(Arrays.asList(4, 3, 3, 3, 2, 1, 1, 1));
         assertEquals(4, numbers.size());
         assertArrayEquals(new Object[]{1,2,3,4}, numbers.toArray()); //sort automatically ??
+
+        Set<String> set = new HashSet<>(){{add("a"); add("b"); add("c");}};
     }
 
     @Test
