c324a3d68e451a0be61be2c8c935b3d6ee5f7030
ruby.sun <ruby.sun@cgi.com>
2019-04-29 16:19:17 -0300

    add exception

---
+++ src/main/java/com/ruby/sun/exception/JException.java
@@ -0,0 +1,49 @@
+package com.ruby.sun.exception;
+
+import org.junit.Test;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class JException {
+    /**
+     * A try-with-resources block can still have the catch and finally blocks
+     * which will work in the same way as with a traditional try block.
+     */
+    @Test
+    public void testTryWithResource() {
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new File("test.txt"));
+            while (scanner.hasNext()) {
+                System.out.println(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+        //equivalent to
+        try(Scanner scanner2 = new Scanner(new File(""))) {
+            while (scanner.hasNext()) {
+                System.out.println(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testTryWithMultipleResource(){
+        try (Scanner scanner = new Scanner(new File("testRead.txt"));
+             PrintWriter writer = new PrintWriter(new File("testWrite.txt"))) {
+            while (scanner.hasNext()) {
+                writer.print(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
