3e90bf9ca70fbaebe8fe3e133d7efb04ba873fbf
ruby.sun <ruby.sun@cgi.com>
2019-05-27 17:12:01 -0300

    update

--- src/main/java/com/ruby/sun/datastructure/array/Array.java
+++ src/main/java/com/ruby/sun/datastructure/array/Array.java
@@ -1,26 +1,73 @@
 package com.ruby.sun.datastructure.array;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.util.Arrays;
 
 import org.junit.Test;
 
 public class Array {
-	@Test
-	public void test() {
-		assertEquals(0, arrayPairSum(new int[] {}));
-		assertEquals(4, arrayPairSum(new int[] {1, 4, 3, 2}));
-		assertEquals(7, arrayPairSum(new int[] {7}));
-		assertEquals(6, arrayPairSum(new int[] {4, 4, 3, 2}));
-		assertEquals(18, arrayPairSum(new int[] {1, 4, 6, 10, 7, 8, 3, 2}));
-	}
-	
-	public static int arrayPairSum(int[] nums) {
+    @Test
+    public void test() {
+//		assertEquals(0, arrayPairSum(new int[] {}));
+//		assertEquals(4, arrayPairSum(new int[] {1, 4, 3, 2}));
+//		assertEquals(7, arrayPairSum(new int[] {7}));
+//		assertEquals(6, arrayPairSum(new int[] {4, 4, 3, 2}));
+//		assertEquals(18, arrayPairSum(new int[] {1, 4, 6, 10, 7, 8, 3, 2}));
+//      assertArrayEquals(new int[]{2, 13, 3, 11, 5, 17, 7}, increasingOrderCards(new int[]{17, 13, 11, 2, 3, 5, 7}));
+        assertArrayEquals(new int[]{2, 4, 6, 7, 1, 3}, sortArrayByParity(new int[]{3, 1, 2, 4, 7, 6}));
+    }
+
+    public int fib(int N) {
+        int[] output = new int[N];
+        return output[N - 1];
+    }
+
+    public int[] sortArrayByParity(int[] nums) {
+        int[] output = new int[nums.length];
+        int j = 0;
+        int k = nums.length - 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                output[j++] = nums[i];
+            } else {
+                output[k--] = nums[i];
+            }
+        }
+        return output;
+    }
+
+    public int[] increasingOrderCards(int[] nums) {
+        int[] output = new int[nums.length];
+        Arrays.sort(nums);
+        Arrays.fill(output, -1);
+        boolean skip = false;
+        int j = 0;
+        int i = 0;
+        while (i < nums.length) {
+            if (output[j] == -1) {
+                if (!skip) {
+                    output[j] = nums[i];
+                    i++;
+                    skip = true;
+                } else {
+                    skip = false;
+                }
+            }
+            j++;
+            if (j == nums.length) {
+                j = 0;
+            }
+        }
+        System.out.println(Arrays.toString(output));
+        return output;
+    }
+
+    public static int arrayPairSum(int[] nums) {
         int sum = 0;
         Arrays.sort(nums);
-        for(int i=0; i< nums.length-1; i=+2) {
-        	sum += nums[i];
+        for (int i = 0; i < nums.length - 1; i = +2) {
+            sum += nums[i];
         }
         return sum;
     }
--- src/main/java/com/ruby/sun/datastructure/sort/Sort.java
+++ src/main/java/com/ruby/sun/datastructure/sort/Sort.java
@@ -5,44 +5,114 @@ import static org.junit.Assert.assertArrayEquals;
 import org.junit.Test;
 
 public class Sort {
-	@Test
-	public void test() {
-		assertArrayEquals(new int[] {5, 7, 9, 10, 15}, insertionSort(new int[] {9, 7, 15, 5, 10}));
-		assertArrayEquals(new int[] {5, 5, 7}, insertionSort(new int[] {5, 7, 5}));
-	}
-	
-	public int[] quickSort(int[] nums) {
-		return nums;
-	}
-	
-	public int[] insertionSort(int[] nums) {
-		int j;
-		for(int i=1; i< nums.length; i++) {
-			j = i;
-			while(j>0 && nums[j] < nums[j-1]) {
-				int temp = nums[j];
-				nums[j] = nums[j-1];
-				nums[j-1] = temp;
-				j = j - 1;
-			}
-		}
-		return nums;
-	}
-	
-	public void selectionSort(int[] nums) {
-		for(int i=nums.length-1; i>=0; i--) {
-			int cur = 0;
-			for(int j=1; j<=i; j++) {
-				if(nums[cur] < nums[j]) {
-					cur = j;
-				}
-			}
-			if(cur != i) {
-				int temp = nums[i];
-				nums[i] = nums[cur];
-				nums[cur] = temp;
-			}
-		}
-	}
-	
+    @Test
+    public void test() {
+        int[] array = new int[]{9, 7, 15, 5, 10};
+        quickSort(array, 0, array.length - 1);
+        assertArrayEquals(new int[]{5, 7, 9, 10, 15}, array);
+    }
+
+    public void mergeSort(int[] nums, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+
+            mergeSort(nums, l, m);
+            mergeSort(nums, m + 1, r);
+
+            merge(nums, l, m, r);
+        }
+    }
+    private void merge(int[] nums, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+
+        System.arraycopy(nums, l, left, 0, n1);
+        System.arraycopy(nums, m+1, right, 0, n2);
+
+        int i = 0;
+        int j = 0;
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (left[i] <= right[j]) {
+                nums[k] = left[i];
+                i++;
+            } else {
+                nums[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            nums[k] = left[i];
+            i++;
+            k++;
+        }
+        while (j < n2) {
+            nums[k] = right[j];
+            j++;
+            k++;
+        }
+    }
+
+    public void quickSort(int[] nums, int low, int high) {
+        if (low < high) {
+            int pi = partition(nums, low, high);
+            quickSort(nums, low, pi - 1);
+            quickSort(nums, pi + 1, high);
+        }
+    }
+
+    private int partition(int[] nums, int low, int high) {
+        int pivot = nums[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (nums[j] <= pivot) {
+                i++;
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+            }
+        }
+
+        int temp = nums[i + 1];
+        nums[i + 1] = nums[high];
+        nums[high] = temp;
+
+        return i + 1;
+    }
+
+    public int[] insertionSort(int[] nums) {
+        int j;
+        for (int i = 1; i < nums.length; i++) {
+            j = i;
+            while (j > 0 && nums[j] < nums[j - 1]) {
+                int temp = nums[j];
+                nums[j] = nums[j - 1];
+                nums[j - 1] = temp;
+                j = j - 1;
+            }
+        }
+        return nums;
+    }
+
+    public void selectionSort(int[] nums) {
+        for (int i = nums.length - 1; i >= 0; i--) {
+            int cur = 0;
+            for (int j = 1; j <= i; j++) {
+                if (nums[cur] < nums[j]) {
+                    cur = j;
+                }
+            }
+            if (cur != i) {
+                int temp = nums[i];
+                nums[i] = nums[cur];
+                nums[cur] = temp;
+            }
+        }
+    }
+
 }
