f4066fc2b5c45a2224f676b7048206a800467027
Rui Sun <ruisun@s-MacBook-Air.lan>
2019-05-29 22:50:38 -0300

    update

--- src/main/java/com/ruby/sun/datastructure/string/JString2.java
+++ src/main/java/com/ruby/sun/datastructure/string/JString2.java
@@ -1,5 +1,6 @@
 package com.ruby.sun.datastructure.string;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -19,72 +20,38 @@ public class JString2 {
 	}
 	//https://leetcode.com/problems/valid-palindrome-ii/
 	public boolean validPalindrome(String s) {
-		if (isPalindrome(s))
-			return true;
-		int i = 0;
-		int j = s.length() - 1;
-		while (s.charAt(i) == s.charAt(j)) {
-			i++;
-			j--;
-		}
-		int rightDelete = 0;
-		int leftDelete = 0;
-		for (int k = i; k <= j; k++) {
-			if (!isPalindrome(s.substring(k, j + 1))) {
-				leftDelete++;
-				if (leftDelete > 1)
-					break;
-			} else
-				break;
-		}
-		for (int k = j + 1; k > i; k--) {
-			if (!isPalindrome(s.substring(i, k))) {
-				rightDelete++;
-				if (rightDelete > 1)
-					break;
-			} else
-				break;
-		}
-		int minDelete = Math.min(rightDelete, leftDelete);
-		return minDelete <= 1;
-	}
-	private boolean isPalindrome(String s) {
 		int i = 0;
 		int j = s.length() - 1;
 		while (i <= j && s.charAt(i) == s.charAt(j)) {
 			i++;
 			j--;
 		}
-		return i > j;
+		if (i > j) {
+			return true;
+		}
+		int pos = i;
+		if (i + 1 <= j && s.charAt(i + 1) == s.charAt(j)) {
+			i++;
+			while (i <= j && s.charAt(i) == s.charAt(j)) {
+				i++;
+				j--;
+			}
+			if (i > j) {
+				return true;
+			}
+		}
+		i = pos;
+		j = s.length() - 1 - i;
+		if (j - 1 >= i && s.charAt(j - 1) == s.charAt(i)) {
+			j--;
+			while (j >= i && s.charAt(j) == s.charAt(i)) {
+				i++;
+				j--;
+			}
+			if (i > j) {
+				return true;
+			}
+		}
+		return false;
 	}
-
-	// public boolean validPalindrome(String s) {
-	// int i = 0;
-	// int j = s.length() - 1;
-	// while (i <= j && s.charAt(i) == s.charAt(j)) {
-	// i++;
-	// j--;
-	// }
-	// if (i > j) { return true; }
-	// int pos = i;
-	// if (i+1<=j && s.charAt(i + 1) == s.charAt(j)) {
-	// i++;
-	// while (i <= j && s.charAt(i) == s.charAt(j)) {
-	// i++;
-	// j--;
-	// }
-	// if (i > j) { return true; }
-	// }
-	// i = pos;
-	// j = s.length() - 1 - i;
-	// if (j-1>=i && s.charAt(j - 1) == s.charAt(i)) {
-	// j--;
-	// while (j >= i && s.charAt(j) == s.charAt(i)){
-	// i++;
-	// j--;
-	// }
-	// if (i > j) { return true; }
-	// }
-	// return false;
-	// }
 }
